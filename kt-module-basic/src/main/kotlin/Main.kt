eda0b04579f0a4cd81edb7467058acd3d801be52
man-chi <manchi@waxc.tech>
2024-02-15 22:06:28 +0800

    add kt module

---
+++ kt-module-basic/src/main/kotlin/Main.kt
@@ -0,0 +1,46 @@
+package tech.waxc
+
+fun main() {
+    println("Hello World123!")
+    add(1, 3)
+    sub(2, 3)
+    multiply(2, 3)
+}
+
+fun add(a: Int, b: Int) {
+    println("not implement")
+}
+
+fun sub(a: Int, b: Int) {
+    println("not impl")
+}
+
+/**
+ * Multiplies two integers.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The result of multiplying a and b.
+ */
+
+/**
+ * Multiplies two integers.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The result of multiplying a and b.
+ */
+fun multiply(a: Int, b: Int): Int {
+    val result = a * b
+    println("Multiplying $a and $b to get $result")
+    return result
+}
+
+
+//function to divide two numbers
+fun divide(a: Int, b: Int): Int {
+    require(b != 0) { "Cannot divide by zero" }
+    val result = a / b
+    println("Dividing $a and $b to get $result")
+    return result
+}
